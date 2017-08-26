1bcc30ba17b2a93fbaf31dae85a823c4e6a5913b
diptesh03 <diptesh03@gmail.com>
2017-08-26 16:48:08 +0530

    Audit screen

---
+++ Panchayat/Controllers/AuditsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Panchayat;
+using PagedList;
+
+namespace Panchayat.Controllers
+{
+    public class AuditsController : Controller
+    {
+        private PanchayatEntities db = new PanchayatEntities();
+
+        // GET: Audits
+        public ActionResult Index(int? page,int? No,int? Year)
+        {
+            var audits = db.Audits.Where(x=>x.AuditNo >0);
+       
+            if (No!=null)
+            {
+                audits = audits.Where(a=>a.AuditNo==No);
+
+                page = 1;
+            }
+            if (Year != null)
+            {
+                audits = audits.Where(a => a.Year == Year);
+
+                page = 1;
+            }
+            ViewBag.YearBox = MyExtensions.MakeYrRq(8, 1, DateTime.Today.Year);
+            int pageSize = db.Configs.FirstOrDefault().RowsPerPage ?? 5;
+            int pageNumber = (page ?? 1);
+            return View(audits.ToList().ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Audits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(audit);
+        }
+
+        // GET: Audits/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Audits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AuditID,AuditNo,ListOfAuditObjection,ActionsTaken,Year")] Audit audit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Audits.Add(audit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(audit);
+        }
+
+        // GET: Audits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(audit);
+        }
+
+        // POST: Audits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AuditID,AuditNo,ListOfAuditObjection,ActionsTaken,Year")] Audit audit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(audit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(audit);
+        }
+
+        // GET: Audits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(audit);
+        }
+
+        // POST: Audits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Audit audit = db.Audits.Find(id);
+            db.Audits.Remove(audit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Panchayat/PanchayatMdl.Designer.cs
+++ Panchayat/PanchayatMdl.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Erryl Alvares\Documents\Visual Studio 2017\Projects\PanchT\Panchayat\PanchayatMdl.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Projects\Panchayat\Panchayat\PanchayatMdl.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
