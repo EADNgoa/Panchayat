fbff0ef7d4f266e64fafe1a0e731cb86bacb81b5
Erryl Alvares <erryl1@hotmail.com>
2017-08-31 19:48:54 +0530

    Revert "InventoryBasics"
    This reverts commit 993c63efb11e14dcd5e988d420f459c9f46b6f82.

--- Panchayat/RegisterType.cs
+++ Panchayat/RegisterType.cs
@@ -21,8 +21,6 @@ namespace Panchayat
             this.IllegalConstructions = new HashSet<IllegalConstruction>();
             this.Inwards = new HashSet<Inward>();
             this.Outwards = new HashSet<Outward>();
-            this.InOutRegsIssues = new HashSet<InOutRegsIssue>();
-            this.InOutRegsRecpts = new HashSet<InOutRegsRecpt>();
         }
     
         public int RegisterTypeID { get; set; }
@@ -36,9 +34,5 @@ namespace Panchayat
         public virtual ICollection<Inward> Inwards { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Outward> Outwards { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<InOutRegsIssue> InOutRegsIssues { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<InOutRegsRecpt> InOutRegsRecpts { get; set; }
     }
 }
