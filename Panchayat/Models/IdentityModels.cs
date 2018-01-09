c229546a72d8fa4c51555373502a81f9dc2060b2
diptesh03 <diptesh03@gmail.com>
2018-01-09 11:41:07 +0530

    no message

--- Panchayat/Models/IdentityModels.cs
+++ Panchayat/Models/IdentityModels.cs
@@ -31,5 +31,9 @@ namespace Panchayat.Models
         }
 
         public System.Data.Entity.DbSet<Panchayat.Notice> Notices { get; set; }
+
+        public System.Data.Entity.DbSet<Panchayat.Holiday> Holidays { get; set; }
+
+        public System.Data.Entity.DbSet<Panchayat.LeaveType> LeaveTypes { get; set; }
     }
 }
